0c6016a0a7c6112c074d8135ce3245866a8e84c3
Ganesh-Chougale <gchougale32@gmail.com>
2024-07-17 18:37:42 +0530

    day 1 Challenge Complete

---
+++ Day 01 Variable and Data Types/variableDatatype.js
@@ -0,0 +1,40 @@
+// Activity 1 : Variable Declaration
+var numVal = 23;
+console.log(numVal);
+
+let strVal = "Some String";
+console.log(strVal);
+
+// Activity 2: Constant Declaration
+const boolVal = true;
+console.log(boolVal);
+
+// Activity 3: Data Types
+let num1 = 34;
+let str1 = "Again Some String";
+let bool1 = false;
+let obj1 = {name: "Ganesh"}
+let arr1 = ["Hey", "There", "Everyone"];
+let fn1 = str2 => str2;
+
+console.log(typeof(num1));
+console.log(typeof(str1));
+console.log(typeof(bool1));
+console.log(typeof(obj1));
+console.log(typeof(arr1));
+console.log(typeof(fn1));
+
+// Activity 4: Reassigning Variables
+let isonline;
+isonline = false;
+console.log("is user online? : ", isonline);
+isonline = true;
+console.log("is user online? : ", isonline);
+
+// Activity 5: Understaring Const
+const day1Complete = true;
+console.log("Day 1 Challege Complete : ", day1Complete);
+day1Complete = false;
+console.log("Day 1 Challege Complete : ", day1Complete);
+
+
